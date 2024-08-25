2c3151ebd11bb0fd9a31d69d51667da4e01177c5
Drihmia <drihmia.redouane@gmail.com>
2024-08-25 08:59:03 +0000

    update swagger config file to use our server's baseUrl instead of localhost

--- config/swagger.js
+++ config/swagger.js
@@ -9,7 +9,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'http://localhost:3000',
+      url: 'https://drihmia.tech',
       description: 'Development server',
     },
   ],
