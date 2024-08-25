78e6e23541b82a97651e617162be92f0516602d3
Drihmia <drihmia.redouane@gmail.com>
2024-08-25 17:25:39 +0100

    refactor: Fix Redis key/set operation issue after upgrading to 4.7.0
     The key/set operation was getting stuck after upgrading Redis from 2.8.0 to 4.7.0. The issue was resolved by switching to an async/await approach for setting values.
    
     + createDev.js exit with code 1 instead of zero if something wrongs happen

--- createDev.js
+++ createDev.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import devService from './services/DevService';
-import DBConnection from './config/db.js';
+import devService from './services/devService';
+import DBConnection from './config/db';
 
 dotenv.config();
 DBConnection();
@@ -10,12 +10,12 @@ DBConnection();
   const email = process.env.EMAIL;
   if (!email || !password) {
     console.log('Please provide email and password');
-    process.exit(0);
+    process.exit(1);
   }
   const devFromDb = await devService._getDevByEmail(email);
   if (devFromDb) {
     console.log(`${devFromDb.email} already exists`);
-    process.exit(0);
+    process.exit(1);
   }
   const dev = await devService._createDev({ email, password });
   console.log('Dev created:\n', dev);
